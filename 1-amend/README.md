492b65b8784ddd7ed6d3d5cd85a1153152e6b160
panic-pomelo <manuel.panichelli@pomelo.la>
2024-09-03 12:50:46 -0300

    feat: adapt ex 1

--- 1-amend/README.md
+++ 1-amend/README.md
@@ -1,5 +1,5 @@
 # Ejercicio 1 - Amend
 
-1. Corregir el mensaje del commit `"sTe comyt tiene taipos"`
-2. Editar el archivo `example.txt` y agregar el cambio al commit corregido.
+1. Corregir el mensaje del commit `"agreo nuev fetur"` para que siga conventional commits y no tenga typos
+2. Agregar una nueva línea (cualquiera) al archivo `example.txt` y agregar el cambio al commit corregido.
 3. Pushear y que quede en sync con origin.
--- 1-amend/setup.sh
+++ 1-amend/setup.sh
@@ -3,10 +3,10 @@ rm -rf exercise/
 rm -rf exercise-origin.git
 
 echo "## Creating origin"
-git -c init.defaultBranch="master" init --bare exercise-origin.git
+git -c init.defaultBranch="main" init --bare exercise-origin.git
 
 echo "## Creating local"
-git -c init.defaultBranch="master" init exercise
+git -c init.defaultBranch="main" init exercise
 cd exercise
 git remote add origin ../exercise-origin.git
 git commit --allow-empty -m "Initial commit"
@@ -16,11 +16,8 @@ echo "## Commiting changes"
 touch "example.txt"
 
 echo "initial text" >> example.txt
+echo "some text" >> example.txt
 git add .
-git commit -m "Edito example.txt"
-
-echo "some text" >> "example.txt"
-git add .
-git commit -m "sTe comyt tiene taipos"
+git commit -m "agreo nuev fetur"
 
 git push
